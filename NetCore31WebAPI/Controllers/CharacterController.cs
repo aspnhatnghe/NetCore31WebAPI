0eb0fcc05e16054111a0382eab14c264f0c11ef5
Hien Luong <hien.th.luong@dektech.com.au>
2020-03-01 19:53:17 +0700

    Add project files.

---
+++ NetCore31WebAPI/Controllers/CharacterController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCore31WebAPI.Models;
+using NetCore31WebAPI.Services.Interfaces;
+
+namespace NetCore31WebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(_characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(_characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character newCharacter)
+        {
+            return Ok(_characterService.AddCharacter(newCharacter));
+        }
+
+        /*
+        private static Character knight = new Character();
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character { Name = "Sam"}
+        };
+
+        public IActionResult Get()
+        {
+            //return Ok(knight);
+            return Ok(characters);
+        }
+
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
+        */
+    }
+}
---
+++ NetCore31WebAPI/Services/Interfaces/ICharacterService.cs
@@ -0,0 +1,15 @@
+﻿using NetCore31WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore31WebAPI.Services.Interfaces
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+        Character GetCharacterById(int id);
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
